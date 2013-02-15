ad952f7cd2e5d7777825b495b2973c5adb6dd496
Tyler Mandry <tmandry@gmail.com>
2013-02-14 23:22:10 -0600

    Update test_table. It compiles.

--- test/test_table.cpp
+++ test/test_table.cpp
@@ -3,6 +3,8 @@
 #include "../Error.h"
 #include <string>
 #include <vector>
+#include <array>
+#include <utility>
 
 using namespace std;
 using namespace ::testing;
@@ -11,13 +13,17 @@ class TableTest : public Test
 {
 protected:
   virtual void SetUp() {
-    string attr_array[] = { "Name", "Age", "Grade", "GPA", "GraduationDate", "BooksOwed", "DiningDollars", "LastLoginDate" };
-    auto attribute_names = vector<string>(attr_array, attr_array+8);
-    Table::TYPE type_array[] = { Table::STRING, Table::INT, Table::STRING, Table::FLOAT, Table::DATE, Table::INT, Table::FLOAT, Table::DATE };
-    auto attribute_types = vector<Table::TYPE>(type_array, type_array + 8);
+    shared_attributes.push_back(make_pair("Name", Table::STRING));
+    shared_attributes.push_back(make_pair("Age",  Table::INT));
+    shared_attributes.push_back(make_pair("Grade", Table::STRING));
+    shared_attributes.push_back(make_pair("GPA", Table::FLOAT));
+    shared_attributes.push_back(make_pair("GraduationDate", Table::DATE));
+    shared_attributes.push_back(make_pair("BooksOwed", Table::INT));
+    shared_attributes.push_back(make_pair("DiningDollars", Table::FLOAT));
+    shared_attributes.push_back(make_pair("LastLoginDate", Table::DATE));
 
-    string jane_array[] = { "Jane Smith", "19", "Sophomore", "2.8", "2015/05/06", "-2", "-1.28", "01:44:12" };
-    single_record = vector<string>(jane_array, jane_array + 5);
+    RowArray jane_array = { "Jane Smith", "19", "Sophomore", "2.8", "2015/05/06", "-2", "-1.28", "01:44:12" };
+    single_record = vector<string>(jane_array.begin(), jane_array.end());
   }
 
   virtual void TearDown() {
@@ -26,34 +32,36 @@ protected:
   }
 
   void setup_shared_table() {
-    shared_table = Table(attribute_names, attribute_types);
+    shared_table = Table(shared_attributes);
   }
 
+  // returns number of rows
   int setup_shared_table_with_data() {
     setup_shared_table();
 
-    string jane_array[] = { "Jane Smith", "19", "Sophomore", "2.8", "2015/05/06", "-2", "-1.28", "01:44:12" };
-    vector<string> jane(jane_array, jane_array + 5);
+    RowArray jane_array = { "Jane Smith", "19", "Sophomore", "2.8", "2015/05/06", "-2", "-1.28", "01:44:12" };
+    vector<string> jane(jane_array.begin(), jane_array.end());
 
-    string jack_array[] = { "Jack Smith", "21", "Senior",    "3.2", "2013/05/06", "4",  "67.45", "14:56:34" };
-    vector<string> jack(jack_array, jack_array + 5);
+    RowArray jack_array = { "Jack Smith", "21", "Senior",    "3.2", "2013/05/06", "4",  "67.45", "14:56:34" };
+    vector<string> jack(jack_array.begin(), jack_array.end());
 
-    string jim_array[] =  { "Jim Smith",  "21", "Senior",    "3.1", "2013/05/06", "-5", "20.19", "21:00:00" };
-    vector<string> jim(jim_array, jim_array + 5);
+    RowArray jim_array =  { "Jim Smith",  "21", "Senior",    "3.1", "2013/05/06", "-5", "20.19", "21:00:00" };
+    vector<string> jim(jim_array.begin(), jim_array.end());
 
-    string bill_array[] = { "Bill Smith", "NULL","Junior",   "3.1", "2013/05/06", "-5", "20.19", "21:00:00" };
-    vector<string> bill(bill_array, bill_array + 5);
+    RowArray bill_array = { "Bill Smith", "NULL","Junior",   "3.1", "2013/05/06", "-5", "20.19", "21:00:00" };
+    vector<string> bill(bill_array.begin(), bill_array.end());
 
     shared_table.insert(jane);
     shared_table.insert(jack);
     shared_table.insert(jim);
     shared_table.insert(bill);
-    return 3;
+    return 4;
   }
 
+  typedef array<string, 8> RowArray;
+
   Table shared_table;
-  vector<string> attribute_names;
-  vector<Table::TYPE> attribute_types;
+  vector<pair<string, Table::TYPE>> shared_attributes;
   vector<string> single_record;
 };
 
@@ -74,21 +82,17 @@ TEST_F(TableTest, ConstructorCreatesPredefinedTableWithNoRows) {
 TEST_F(TableTest, AddWorks) {
   setup_shared_table();
   int attributes_size = shared_table.attributes().size();
-  EXPECT_TRUE(shared_table.add("72", Table::INT));
+  EXPECT_TRUE(shared_table.add("AnotherColumn", Table::INT));
   EXPECT_EQ(shared_table.attributes().size(), attributes_size+1);
-}
-
-TEST_F(TableTest, AddFailsOnNonIntegerString) {
-  setup_shared_table();
-  EXPECT_FALSE(shared_table.add("lala", Table::INT));
+  EXPECT_THAT(shared_table.attributes().back(), Pair("AnotherColumn", Table::INT));
 }
 
 /* Test Deleting attributes from Table */
 TEST_F(TableTest, DeleteAttributeWorks) {
   setup_shared_table();
-  EXPECT_THAT(shared_table.attributes(), Contains(Key("GraduationDate")));
+  EXPECT_THAT(shared_table.attributes(), Contains( Pair("GraduationDate", _) ));
   EXPECT_TRUE(shared_table.deleteAttribute("GraduationDate"));
-  EXPECT_THAT(shared_table.attributes(), Not(Contains(Key("GraduationDate"))));
+  EXPECT_THAT(shared_table.attributes(), Not(Contains( Pair("GraduationDate", _) )));
 }
 
 TEST_F(TableTest, DeleteAttributeFailsForNonexistentAttribute) {
@@ -113,13 +117,10 @@ TEST_F(TableTest, InsertFailsForWrongVectorSize) {
 }
 
 /* Test retrieving attributes */
-// TODO pending changes
-/*
 TEST_F(TableTest, AttributesReturnsAttributesList) {
   setup_shared_table();
-  EXPECT_THAT(shared_table.attributes(), ContainerEq(attribute_names));
+  EXPECT_THAT(shared_table.attributes(), ContainerEq(shared_attributes));
 }
-*/
 
 /* Test size correctness */
 TEST_F(TableTest, SizeReturnsNumberOfTableEntries) {
@@ -139,32 +140,22 @@ TEST_F(TableTest, RenameFailsForNonexistentAttribute) {
 }
 
 /* Test the joining of two tables */
-// TODO pending changes
-/*
 TEST_F(TableTest, CrossJoinHasAllAttributes) {
   setup_shared_table();
 
-  vector<string> names;
-  vector<Table::TYPE> types;
-  names.push_back("abc"); types.push_back(Table::DATE);
-  names.push_back("def"); types.push_back(Table::INT);
-  names.push_back("ghi"); types.push_back(Table::STRING);
-  Table other_table(names, types);
+  vector<pair<string, Table::TYPE>> attributes;
+  attributes.push_back(make_pair("abc", Table::DATE));
+  attributes.push_back(make_pair("def", Table::INT));
+  attributes.push_back(make_pair("ghi", Table::STRING));
+  Table other_table(attributes);
+
   Table joined = shared_table.crossJoin(other_table);
 
-  vector<Table::TYPE> attribute_types = joined.attributes();
-  vector<Table::TYPE> join_types(types);
-  join_types.insert(join_types.end(), attribute_types.begin(), attribute_types.end());
-  sort(join_types.begin(), join_types.end());
-  EXPECT_THAT(attribute_types, WhenSorted(ContainerEq(join_types)));
-
-//  vector<string> attributes = joined.attributes();
-//  vector<string> join_names(names);
-//  join_names.insert(join_names.end(), attribute_names.begin(), attribute_names.end());
-//  sort(join_names.begin(), join_names.end());
-//  EXPECT_THAT(attributes, WhenSorted(ContainerEq(join_names)));
+  vector<pair<string, Table::TYPE>> join_attributes(shared_attributes);
+  join_attributes.insert(join_attributes.end(), attributes.begin(), attributes.end());
+  sort(join_attributes.begin(), join_attributes.end());
+  EXPECT_THAT(joined.attributes(), ContainerEq(join_attributes));
 }
-*/
 
 /* Counting of attributes */
 TEST_F(TableTest, CountAttributeWorks) {

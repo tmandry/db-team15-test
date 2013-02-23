7c3bbcfa0a32b4e40ec37c9c5214eb0deb1d8371
Patrick Casey <patrickcasey2014@gmail.com>
2013-02-23 13:26:54 -0600

    Created reataurantapp files
    I started this and realized it is difficult to go much further without
    seeing the API/the changes that were made since testing...

---
+++ restaurantapp.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Team15Database.dll"
+#include "restaurantapp.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Welcome to the Restaurant Database Application!\n"
+		 << "*********************************************************\n"
+		 << "Please enter your query: ";
+}
---
+++ restaurantapp.h
@@ -0,0 +1,7 @@
+#include "Team15Database.dll"
+
+/**
+	Create a database from data contained in .txt files
+	
+*/
+void input_data();

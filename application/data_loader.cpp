7fedd4aa9e9067cf5ee1343c9e40054a80fc5d48
Tyler Mandry <tmandry@gmail.com>
2013-02-27 00:28:48 -0600

    Add to DataLoader

--- application/data_loader.cpp
+++ application/data_loader.cpp
@@ -12,7 +12,34 @@ struct TableDescriptor {
   typedef array<tuple<string, Table::TYPE>, Num> type;
 };
 
-static const TableDescriptor<2>::type MY_TABLE = {
-  make_tuple("name", Table::STRING),
-  make_tuple("age", Table::INT)
+static const TableDescriptor<2>::type CM_ACCEPTS = {
+  make_tuple("placeID", Table::STRING),
+  make_tuple("Rpayment", Table::STRING)
 };
+
+static const TableDescriptor<2>::type CM_CUISINE = {
+  make_tuple("placeID", Table::STRING),
+  make_tuple("Rcuisine", Table::STRING)
+};
+
+static const TableDescriptor<3>::type CM_HOURS = {
+  make_tuple("placeID", Table::STRING),
+  make_tuple("hours", Table::STRING),
+  make_tuple("days", Table::STRING)
+};
+
+static const TableDescriptor<2>::type CM_PARKING = {
+  make_tuple("placeID", Table::STRING),
+  make_tuple("parking_lot", Table::STRING)
+};
+
+DataLoader::DataLoader(string data_dir) {
+
+}
+
+DataLoader::~DataLoader() {}
+
+Database* DataLoader::load() {
+  Database *db = new Database;
+  return db;
+}
